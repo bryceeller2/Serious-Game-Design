0b4441a54521aaed3803ebf8f78f6a7602526ef4
Bryce Eller <BryceEller@drexel.edu>
2019-11-19 17:12:05 -0500

    First Upload for Ardy the Arduino Game

---
+++ Assets/Scripts/BrycesScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class BrycesScript : MonoBehaviour
+{
+    public float speed;
+    private float amountToMove;
+    public string value;
+
+    SerialPort sp = new SerialPort("COM5", 9600); //Change this based on which USB port you're using
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        sp.Open();
+        sp.ReadTimeout = 1;
+        print("Bryce's Script is listening for Arduino Input");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        amountToMove = speed * Time.deltaTime;
+
+        if (sp.IsOpen)
+        {
+            try
+            {
+                value = sp.ReadExisting();
+                value = value.Remove(3);
+                MoveObject(value);
+            }
+            catch (System.Exception)
+            {
+
+            }
+        }
+    }
+
+    void MoveObject(string Direction)
+    {
+        string output = "Input Sensed: " + Direction +" = ";
+        if (Direction == "100")
+        {
+            output += "Left";
+            transform.Translate(Vector3.left * amountToMove, Space.World);
+        }
+        else if (Direction == "010")
+        {
+            output += "Jump";
+            //transform.Translate(Vector3.right * amountToMove, Space.World);
+        }
+        else if (Direction == "001")
+        {
+            output += "Right";
+            transform.Translate(Vector3.right * amountToMove, Space.World);
+        }
+        else
+        {
+            output += "No Action";
+        }
+        print(output);
+    }
+}
