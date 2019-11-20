b82f95cc104c05ae5e44c979f940500e5336beb0
Bryce Eller <BryceEller@drexel.edu>
2019-11-20 11:26:07 -0500

    Collisions Work

---
+++ Assets/Scripts/EnableDisable.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class EnableDisable : MonoBehaviour
+{
+    public float speed;
+    private float amountToMove;
+    public string value;
+    public GameObject redActive;
+
+    //SerialPort sp = new SerialPort("COM5", 9600); //Change this based on which USB port you're using
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //sp.Open();
+        //sp.ReadTimeout = 1;
+        redActive = GameObject.Find("RedActive");
+        print("Bryce's Script is listening for Arduino Input");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            print("weiners");
+            if (redActive.active == true)
+            {
+                redActive.SetActive(false);
+                this.GetComponent<Renderer>().enabled = false;
+                this.GetComponent<BoxCollider2D>().enabled = false;
+            }
+            else
+            {
+                redActive.SetActive(true);
+                this.GetComponent<Renderer>().enabled = true;
+                this.GetComponent<BoxCollider2D>().enabled = true;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            redActive.SetActive(true);
+        }
+    }
+
+    void MoveObject(string Direction)
+    {
+        string output = "Input Sensed: " + Direction +" = ";
+        if (Direction == "100")
+        {
+            output += "Left";
+            transform.Translate(Vector3.left * amountToMove, Space.World);
+        }
+        else if (Direction == "010")
+        {
+            output += "Jump";
+            //transform.Translate(Vector3.right * amountToMove, Space.World);
+        }
+        else if (Direction == "001")
+        {
+            output += "Right";
+            transform.Translate(Vector3.right * amountToMove, Space.World);
+        }
+        else
+        {
+            output += "No Action";
+        }
+        print(output);
+    }
+}
---
+++ Assets/Scripts/KeyMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.x-=1;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.x+=1;
+            this.transform.position = position;
+        }
+
+    }
+}
